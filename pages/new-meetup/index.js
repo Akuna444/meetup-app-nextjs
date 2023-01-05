3dd9594088a3539de5a7da02cbad770ef6b97126
Akuna444 <aklilsolomon3@gmail.com>
2023-01-06 00:34:33 +0300

    add: newMeetupForm

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,9 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+function NewMeetup() {
+  const onAddMeetupHanler = (enteredData) => {
+    console.log(enteredData);
+  };
+  return <NewMeetupForm onAddMeetup={onAddMeetupHanler} />;
+}
+
+export default NewMeetup;
