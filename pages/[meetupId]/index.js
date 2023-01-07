382cd4f66361bc8108d75b24c4330e5179437827
Akuna444 <aklilsolomon3@gmail.com>
2023-01-07 10:39:23 +0300

    add: static path

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,38 @@ function MeetupDetails() {
   );
 }
 
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        src: "https://eadn-wc04-3972048.nxedge.io/wp-content/uploads/2022/04/santamonica.jpg",
+        alt: "first meetup place",
+        title: "First Meetup Place",
+        address: "Some Address 10",
+        description: "The most visted place in the world",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
