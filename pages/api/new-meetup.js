861c246b1c2da08ac8771cd6eaf56bc35060c582
Akuna444 <aklilsolomon3@gmail.com>
2023-01-09 17:10:39 +0300

    connect to mongoDB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://akuna444:<password>@cluster0.ex41jje.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+  }
+}
+
+export default handler;
