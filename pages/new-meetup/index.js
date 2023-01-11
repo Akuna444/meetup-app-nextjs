6e6458e4f283ded0804e6c336de7d13ad821a4aa
Akuna444 <aklilsolomon3@gmail.com>
2023-01-11 21:31:43 +0300

    add post method to mongo db

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function NewMeetup() {
-  const onAddMeetupHanler = (enteredData) => {
-    console.log(enteredData);
+  const router = useRouter();
+
+  const onAddMeetupHandler = async (enteredData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredData),
+      "Content-Type": "application/json",
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
   };
-  return <NewMeetupForm onAddMeetup={onAddMeetupHanler} />;
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
 }
 
 export default NewMeetup;
