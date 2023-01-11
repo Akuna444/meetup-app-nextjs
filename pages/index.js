3b85cb9a36f7ca2ee33f3d72964acd7460afef71
Akuna444 <aklilsolomon3@gmail.com>
2023-01-11 21:40:59 +0300

    getting data from mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,27 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    titles: "A first meetup",
-    image:
-      "https://eadn-wc04-3972048.nxedge.io/wp-content/uploads/2022/04/santamonica.jpg",
-    address: "Some address 10, Los angles",
-    description: "This is my first Meetup",
-  },
-  {
-    id: "m2",
-    titles: "A second meetup",
-    image:
-      "https://firehub.eu/wp-content/uploads/2019/07/640px-Hauptwache_Frankfurt_am_Main.jpg",
-    address: "Some address 11, Frankfurt",
-    description: "This is my second Meetup",
-  },
-];
-
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://akuna444:<password>@cluster0.ex41jje.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = meetupsCollection.find().toArray();
+
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
